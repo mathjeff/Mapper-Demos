f4b86ae92bf8560aa374c22329ff1016efa2cede
Jeffry Gaston <gastoj3@gmail.com>
2025-08-22 15:56:31 +0800

    demo02 downloading, caching, and running Mapper

--- impl/utils.sh
+++ impl/utils.sh
@@ -1,12 +1,32 @@
 # This is a utility script to be sourced by individual demos
 
-function runCommand() {
+function displayCommand() {
   echo "$ $*"
+}
+
+function runCommand() {
+  displayCommand "$*"
   bash -c "$*"
 }
 
 function downloadMapper() {
-  runCommand "curl -L https://github.com/mathjeff/Mapper/releases/download/1.2.0-beta10/x-mapper-1.2.0-beta10.jar -o mapper.jar"
+  # setup
+  fileVersion="1.2.0-beta10"
+  filename="x-mapper-$fileVersion.jar"
+  url="https://github.com/mathjeff/Mapper/releases/download/$fileVersion/$filename"
+  cacheDir="../../impl/cache"
+  mkdir -p "$cacheDir"
+  destFile="$cacheDir/$filename"
+  downloadCommand="curl -L $url -o $destFile"
+  # download Mapper to the cache
+  if [ ! -e "$destFile" ]; then
+    bash -c "$downloadCommand"
+  else
+    # We already downloaded Mapper but it might be more informative to re-display the download command again
+    displayCommand "$downloadCommand"
+  fi
+
+  runCommand cp "$destFile" "mapper.jar"
 }
 
 function pause() {
